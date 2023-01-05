3a102fd43df10e7b0391a8eacc1e02fbd31ec0e0
Brian Noriega <855736+noriega3@users.noreply.github.com>
2023-01-05 02:33:50 -0500

    core: add temp icon references

--- models/lametric.js
+++ models/lametric.js
@@ -1,9 +1,30 @@
 
-const icons = {
-    "N": 51617 
+const icons = { //TODO: search train, there are up/down symbols too
+    "1": 51714,
+    "2": 51715,
+    "3": 51716,
+    "4": 51717,
+    "5": 51718,
+    "6": 51719,
+    "7": 51720,
+    "A": 51721,
+    "C": 51722,
+    "E": 51723,
+    "B": 51724,
+    "D": 51725,
+    "F": 51726,
+    "M": 51728,
+    "L": 51727,
+    "J": 51729,
+    "Z": 51730,
+    "R": 51731,
+    "W": 51732,
+    "Q": 51713,
+    "G": 51712,
+    "N": 51617
 };
 const routeIdToIcon = (routeId) => {
-    return icons[routeId] || 51617;
+    return icons[routeId] || 2451;
 }
 const generateFrame = (text, routeId, index = 0) => {
 
